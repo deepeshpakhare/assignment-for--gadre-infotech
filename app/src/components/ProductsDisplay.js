18278937f4a245e5b99f5ccf0018a6c41bb44457
deepeshpakhare <deepeshpakhare@gmail.com>
2024-09-22 21:09:54 +0530

    feat: data table showing done

--- app/src/components/ProductsDisplay.js
+++ app/src/components/ProductsDisplay.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { getImages, getProducts } from '../utils/fetchFunctions';
-
+import { Table } from 'antd';
+import { render } from '@testing-library/react';
 
 export default function ProductsDisplay() {
     const [products, setProducts] = useState([]);
     const [images, setImages] = useState([]);
+    const [dataSource, setDataSource] = useState([]);
+
+    useEffect(() => {
+        setDataSource(createDataObject({ productsResponse: products, imagesResponse: images }));
+    }, [products, images])
 
     useEffect(() => {
         const fetchProducts = async () => {
@@ -25,28 +31,55 @@ export default function ProductsDisplay() {
         };
         fetchProducts();
         fetchImages();
-        createDataObject({productsResponse: products, imagesResponse: images});
     }, [])
 
-    const createDataObject = ({productsResponse, imagesResponse}) => {
+    const createDataObject = ({ productsResponse, imagesResponse }) => {
         console.log(productsResponse, imagesResponse)
         let result = [];
         for (let obj of productsResponse) {
-            result.push({id: obj.id, prodName: obj.productName, category:obj.category, quantity: obj.quantity});
+            result.push({ key: obj.id, prodName: obj.productName, category: obj.category, quantity: obj.quantity });
         }
-        
+        for (let res of result) {
+            for (let img of imagesResponse) {
+                //console.log(res.id, img.id)
+                if (res.key == img.id) {
+                    //alert("yes");
+                    res["imageUrl"] = img.imageUrl;
+                }
+            }
+        }
+        console.log(result);
+        return result;
     }
+    const columns = [
+        {
+            title: "Image",
+            dataIndex: 'imageUrl',
+            key: 'imageUrl',
+            render: (text) => <img src={text} style={{ width: '50px', height: '50px' }}></img>
+        },
+        {
+            title: 'Product Name',
+            dataIndex: 'prodName',
+            key: 'prodName',
+        },
+        {
+            title: 'Category',
+            dataIndex: 'category',
+            key: 'category',
+        },
+        {
+            title: 'Quantity',
+            dataIndex: 'quantity',
+            key: 'quantity',
+        },
+
+    ];
 
     return (
         <div>
             <h1>Display</h1>
-            <ul>
-                {products.map((prod, index) =>
-                    <li key={index}>
-                       {prod.quantity}
-                    </li>
-                )}
-            </ul>
+            <center><Table dataSource={dataSource} columns={columns} style={{ width: 700 }}/></center>
 
         </div>
     )
