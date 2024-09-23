dfcb5476e1f4a65226ce89111cc1a8673e9ad737
deepeshpakhare <deepeshpakhare@gmail.com>
2024-09-23 08:49:10 +0530

    feat: category selection enabled

--- app/src/components/ProductForm.js
+++ app/src/components/ProductForm.js
@@ -3,7 +3,7 @@ import { Button, Card, Form, Image, Input, Upload, message } from "antd";
 import { AiFillCloseSquare } from "react-icons/ai";
 import { sendData } from '../utils/fetchFunctions';
 import { v4 as uuidv4 } from 'uuid';
-import Password from 'antd/es/input/Password';
+import { Select } from "antd";
 
 
 const formParentStyle = {
@@ -32,8 +32,8 @@ function ProductForm({ name, id, removeSelf, index, setForms, forms, onSubmit },
     useEffect(() => setFormData((prev) => [{ productName, category, id, quantity }]), [productName, category, id, quantity])
 
     const onFinish = (values) => {
-        //console.log("FORM DATA",formData);
-        setFormData((prev) => [{ productName, category, id, quantity}]);
+        //console.log("FORM DATA",values);
+        setFormData((prev) => [{ productName, category, id, quantity }]);
         sendData(formData);
         setTimeout(() => setForms(forms.map((form, formIndex) => index == formIndex ? { id: uuidv4(), name: `form${uuidv4()}` } : form)), 1000);
     };
@@ -48,6 +48,9 @@ function ProductForm({ name, id, removeSelf, index, setForms, forms, onSubmit },
         //console.log('Failed:', errorInfo);
     };
 
+   const handleChange = (value) => {
+        setCategory(value);
+   }
 
     const customRequest = ({ file, onSuccess, onError }) => {
         const formData = new FormData();
@@ -151,55 +154,76 @@ function ProductForm({ name, id, removeSelf, index, setForms, forms, onSubmit },
                             },
                         ]}
                     >
-                        <Input name="category" value={category} onChange={(e) => setCategory(e.target.value)} />
-                        </Form.Item>
-                        <Form.Item
-                            label="quantity"
-                            name="quantityitem"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your password!',
-                                },
-                            ]}
-                        >
-                            <Input name="quantity" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
-                        </Form.Item>
-                        <Form.Item name="upload" label="Upload">
-                            <Upload
-                                customRequest={customRequest}
-                                listType="picture-card"
-                                onChange={handleImageChange}
-                                handlePreview={handlePreview}
-                                disabled={showUpload}
-
-                            > +
-                            </Upload>
-                            {previewImage && (
-                                <Image
-                                    wrapperStyle={{
-                                        display: 'none',
-                                    }}
-                                    preview={{
-                                        visible: previewOpen,
-                                        onVisibleChange: (visible) => setPreviewOpen(visible),
-                                        afterOpenChange: (visible) => !visible && setPreviewImage(''),
-                                    }}
-                                    src={previewImage}
-                                />
-                            )}
-                        </Form.Item>
-                        <Form.Item
-                            wrapperCol={{
-                                offset: 8,
-                                span: 16,
+                        <Select
+                            defaultValue="category1"
+                            style={{
+                                width: 145,
                             }}
-                        >
-                            <Button ref={buttonRef} id={`submitbtn${id}`} type="primary" htmlType="submit" onClick={onFinish}>
-                                Submit
-                            </Button>
+                            onChange={handleChange}
+                            options={[
+                                {
+                                    value: 'category1',
+                                    label: 'Category1',
+                                },
+                                {
+                                    value: 'category2',
+                                    label: 'Category2',
+                                },
+                                {
+                                    value: 'category3',
+                                    label: 'Category3',
+                                },
+                                
+                            ]}
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        label="quantity"
+                        name="quantityitem"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your password!',
+                            },
+                        ]}
+                    >
+                        <Input name="quantity" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
+                    </Form.Item>
+                    <Form.Item name="upload" label="Upload">
+                        <Upload
+                            customRequest={customRequest}
+                            listType="picture-card"
+                            onChange={handleImageChange}
+                            handlePreview={handlePreview}
+                            disabled={showUpload}
 
-                        </Form.Item>
+                        > +
+                        </Upload>
+                        {previewImage && (
+                            <Image
+                                wrapperStyle={{
+                                    display: 'none',
+                                }}
+                                preview={{
+                                    visible: previewOpen,
+                                    onVisibleChange: (visible) => setPreviewOpen(visible),
+                                    afterOpenChange: (visible) => !visible && setPreviewImage(''),
+                                }}
+                                src={previewImage}
+                            />
+                        )}
+                    </Form.Item>
+                    <Form.Item
+                        wrapperCol={{
+                            offset: 8,
+                            span: 16,
+                        }}
+                    >
+                        <Button ref={buttonRef} id={`submitbtn${id}`} type="primary" htmlType="submit" onClick={onFinish}>
+                            Submit
+                        </Button>
+
+                    </Form.Item>
                 </Form>
 
             </Card>
